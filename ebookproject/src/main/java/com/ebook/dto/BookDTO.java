cbfe8f3d46462fc5429f705da0058753611c8c33
bo <3571357@naver.com>
2024-07-19 11:03:14 +0900

    c

---
+++ ebookproject/src/main/java/com/ebook/dto/BookDTO.java
@@ -0,0 +1,35 @@
+package com.ebook.dto;
+
+
+import lombok.*;
+
+import java.util.Base64;
+import java.util.List;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookDTO {
+    private Integer bookNo;
+    private String bookTitle;
+    private String bookGenre;
+    private String bookAuthor;
+    private String bookPublisher;
+    private String bookCategory;
+    private String bookDescription;
+    private byte[] bookImage;
+    private String bookImageUrl;
+    private Integer bookLikeCount;
+    private List<BookChapterDTO> bookChapters;
+
+    public String getBookImageUrl() {
+        try {
+            return Base64.getEncoder().encodeToString(this.bookImage);
+        }catch (Exception e){
+            return null;
+        }
+    }
+}
