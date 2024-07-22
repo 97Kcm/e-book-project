169ba79559f9a205035ef9692c1b035eb4fbee52
bo <3571357@naver.com>
2024-07-22 16:11:51 +0900

    좋아요 및 마이페이지 구현 완료
    book detail 페이지에서 좋아요, 좋아요 취소
    마이페이지 창에서 좋아요 목록 불러오기 및 사용자 정보 불러오기

---
+++ ebookproject-1/src/main/java/com/ebook/mapper/UserMapper.java
@@ -0,0 +1,71 @@
+package com.ebook.mapper;
+
+import com.ebook.dto.BookDTO;
+import com.ebook.dto.user.CashChargeDTO;
+import com.ebook.dto.user.CustomUserDTO;
+import com.ebook.dto.user.UserDTO;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.springframework.security.core.userdetails.User;
+
+import java.util.List;
+
+@Mapper
+public interface UserMapper {
+
+    UserDTO selectUserById(String id);
+
+    void insertUser(UserDTO userDTO);
+
+    UserDTO selectUserByCi(String ci);
+
+    CustomUserDTO selectCustomUserById(String id);
+
+    void insertSocialUser(CustomUserDTO socialUser);
+
+    /******************* 아이디 찾기 *********************/
+
+    UserDTO selectUserByNameAndEmail(
+            @Param("name") String name,
+            @Param("email") String email
+    );
+
+    /******************* 비밀번호 재설정 *******************/
+
+    UserDTO selectUserByIdAndNameAndEmail(
+            @Param("id") String id,
+            @Param("name") String name,
+            @Param("email") String email
+    );
+
+    void resetUserPassword(
+            @Param("id") String id,
+            @Param("name") String name,
+            @Param("email") String email,
+            @Param("password") String EncodingPassword
+    );
+
+    /******************* 마이 페이지 정보 조회 ********************/
+    UserDTO SelectUserInfo(UserDTO userDTO);
+
+
+    /******************** 캐시 충전 ******************************/
+    void insertCashCharge(
+            @Param("userId") String userId,
+            @Param("cashCharge") CashChargeDTO cashCharge
+    );
+    void updateUserCash(
+            @Param("userId") String userId,
+            @Param("cashAmount") Integer cashAmount
+    );
+    /******************** 좋아요 하기 *****************************/
+    List<BookDTO> selectBookByUserLike(String userId);
+    void insertBookLike(
+            @Param("userId") String userId,
+            @Param("bookNo") Integer bookNo
+    );
+    void deleteBookLike(
+            @Param("userId") String userId,
+            @Param("bookNo") Integer bookNo
+    );
+}
