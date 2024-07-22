fbdc9ac822ed2a36394eadbdb5bc8944babebc3a
MisoJ <wjdgnstit97@naver.com>
2024-07-22 18:11:14 +0900

    d
    d

---
+++ ebookproject/src/main/java/com/ebook/controller/MyPageController.java
@@ -0,0 +1,20 @@
+package com.ebook.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class MyPageController {
+    @GetMapping("/mypage")
+    public void mypage() {}
+
+    @PostMapping("/mypage")
+    public void mypagePost() {}
+
+
+
+
+
+
+}
