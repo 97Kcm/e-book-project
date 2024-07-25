33b6addbeb0064ea43457f89986016d3ec2106a1
MisoJ <wjdgnstit97@naver.com>
2024-07-25 10:22:11 +0900

    buybook and change like controller

---
+++ ebookproject/src/main/java/com/ebook/dto/BookBuyDTO.java
@@ -0,0 +1,15 @@
+package com.ebook.dto;
+
+import lombok.*;
+
+@Getter
+@Setter
+@Builder
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class BookBuyDTO {
+    private Integer bookNo;
+
+
+}
