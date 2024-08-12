d941ed23d709c35cd0b57125e833f7c8085808f8
MisoMarie <148054720+MisoMarie@users.noreply.github.com>
2024-08-12 16:07:19 +0900

    Update MyPageController.java

--- ebookproject/src/main/java/com/ebook/controller/MyPageController.java
+++ ebookproject/src/main/java/com/ebook/controller/MyPageController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
-public class MypageController {
+public class MyPageController {
     @GetMapping("/mypage")
     public void mypage() {}
 
